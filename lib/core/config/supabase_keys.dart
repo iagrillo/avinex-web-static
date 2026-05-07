939a5f1da8e3c815a54711b86bdf86af20909c3c
iagrillo <israelgrillo2018@gmail.com>
2026-05-07 19:46:12 +0100

    Update Supabase anon key and rebuild web app

--- lib/core/config/supabase_keys.dart
+++ lib/core/config/supabase_keys.dart
@@ -1,5 +1,5 @@
 abstract final class SupabaseKeys {
   static const String url = 'https://wpummstvwcngrsxdvdvs.supabase.co';
-  static const String anonKey = 'sb_publishable_lMxXUDTmiOh7-fkKxo1Dug_3fZmlDhQ';
+  static const String anonKey = 'eyJhbGciOiJIUzI1NiIsInR5cCI6IkpXVCJ9.eyJpc3MiOiJzdXBhYmFzZSIsInJlZiI6IndwdW1tc3R2d2NuZ3JzeGR2ZHZzIiwicm9sZSI6ImFub24iLCJpYXQiOjE3NzU1OTE2NzAsImV4cCI6MjA5MTE2NzY3MH0.x_o971KwQ1qe5_9WCcSIEouQVUMduGcIzNNOlD7UQgU';
   static const String authRedirectUrl = 'https://avinex-web-static.vercel.app/';
 }
